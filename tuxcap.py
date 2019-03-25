f9848ad64a8b9a5bec72bf839d8bb5217f06d2bb
Kali Singh <kalium@awful.cloud>
2019-03-25 14:41:28 +0200

    add buffering ability

--- tuxcap.py
+++ tuxcap.py
@@ -14,22 +14,20 @@ post_buffer = 100
 
 image_ring = collections.deque("",num_captures);
 
-
 class cam_thread(Thread):   
     
-    should_run = True      # Used to end thread gracefully
-    save_requested = False # Current buffer will be saved when frames_remaining = 0
-    frames_remaining = 0   # Countdown to save buffer when save_requested True
-    lock_requested = False # Prevent new images being dequed when saving
+    should_run = True
+    count = 0
+    frames_remaining = 0
+    save_requested = False
+    lock_requested = False
 
-    def take_pic(self):
+    def take_pic(self): # Returns a single frame
 
-        cam = VideoCapture(0)
-        conf, img = cam.read()
+        cam = VideoCapture(0)  # Set up camera
+        conf, img = cam.read() # Get image
 
         if conf:    # frame captured without any errors
-            print("image taken")
-            #imwrite(name+".jpg",img)
             return(img)
 
 
@@ -38,57 +36,59 @@ class cam_thread(Thread):
         self.count = 0
         self.frames_remaining = 0
         self.save_requested = False
-        self.rock_requested = False
+        self.lock_requested = False
 
-        while self.should_run:
+        while self.should_run: # Allows graceful closeout
             
-            if not(self.lock_requested):
+            if not(self.lock_requested): # Don't take pictures when saving
                 image_ring.append(self.take_pic())
                 time.sleep(frame_period) 
 
-            if self.save_requested:
+            if self.save_requested: # Munge counters when save requested
+
+                print(self.frames_remaining)
                 self.frames_remaining = self.frames_remaining - 1
-                if self.frames_remaining <= 0:
+
+                if self.frames_remaining <= 0: # Pass off to save_buffer
                     self.save_requested = False
                     self.save_buffer_now()
 
 
-
-    def stop_thread(self):
+    def stop_thread(self): # Flip thread maintenance variable
         print("Ending capture thread!")
         self.should_run = False
 
-    def request_buffer(self):
+    def request_buffer(self): # Create countdown to save buffer
         if self.save_requested == False:
             print("start buffer")
-            frames_remaining = post_buffer
+            self.frames_remaining = post_buffer
             self.save_requested = True
         else:
             print("Buffering already in progress")
 
-    def save_buffer_now(self):
+    def save_buffer_now(self): # Create locks and mark buffer for saving
 
         if self.lock_requested == False:
-            self.lock_requested = True
+
+            self.lock_requested = True # prevent new images entering buffer
 
             img_count = 0
-
+            
             dirname = "/home/kalium/code/tuxcap/captures/"+str(int(time.time()))
-
             os.mkdir(dirname)
 
             for image in image_ring:
                 print("saving image "+str(img_count))
-                imwrite(dirname + "/" + str(img_count) + ".jpg",image)
+                imwrite(dirname + "/" + str(img_count) + ".jpg",image) # save
                 img_count = img_count + 1
 
-            self.lock_requested = False
+            self.lock_requested = False  # release lock on buffer
         else:
             print("Buffer presently saving")
 
 
 def main():
-    cam_loop = cam_thread()
+    cam_loop = cam_thread() 
     cam_loop.setDaemon(True)
 
     print("starting camera record loop")
@@ -96,17 +96,21 @@ def main():
 
     ans = ""
 
-    while ans != 'q':
+    while ans != 'q': # command console loop
 
-        ans = input("Tell me something new: ")
+        ans = input("Enter Command >  ")
 
         if ans == "show":
-            print(image_ring)
-            print(len(image_ring))
+            print("there are " + str(len(image_ring)) + " items in the ring")
         elif ans == "cap":
             cam_loop.request_buffer()
+        elif ans in ["help","h","?","wat"]:
+            print("?, h, help, wat: Show this page")
+            print("cap: mark current buffer for capture")
+            print("show: show how many items are currently in buffer")
+            print("q: quit")
         else:
-            print("huh, neat")
+            print("I don't understand. Enter ? for help")
 
     cam_loop.stop_thread();
 
